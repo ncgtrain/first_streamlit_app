7c4d46dfc73b19241ef0cec2ed64ed429916d058
ncgtrain <123206476+ncgtrain@users.noreply.github.com>
2023-01-20 15:31:05 -0500

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -3,6 +3,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast menu')
 
-streamlit.title('Omega 3 7 Blueberry Oatmeal')
-streamlit.title('Kale, Spinach & Rocket Smoothie')
-streamlit.title('Hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 7 Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
