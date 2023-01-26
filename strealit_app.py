9319f25b8cc311709a3cc67987dd4b54c6ff18e5
ncgtrain <123206476+ncgtrain@users.noreply.github.com>
2023-01-26 09:22:51 -0500

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -1,7 +1,7 @@
 import streamlit  
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast menu')
+streamlit.header('Breakfast Favorites')
 
 streamlit.text('🥣Omega 3 7 Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
@@ -13,10 +13,10 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # pick list
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 #table display
 streamlit.dataframe(my_fruit_list)
